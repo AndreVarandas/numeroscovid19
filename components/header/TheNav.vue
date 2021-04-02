e87a844f4f480a52afe9d9ac5c752c729da92303
André Varandas <andre.m.varandas@gmail.com>
2021-04-02 16:38:00 +0100

    feat(components): adds nuxt-property-decorator and basic components

---
+++ components/header/TheNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="nc-nav">
+    <ul>
+      <li>Home</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class TheNav extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.nc-nav {
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="container">
-    <h1 class="title">Hello, world.</h1>
+    <h1 class="title text-white text-6xl">Hello, world.</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Vue } from 'nuxt-property-decorator'
 
-export default Vue.extend({})
+@Component
+export default class Home extends Vue {}
 </script>
 
 <style lang="scss" scoped>
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 </style>
