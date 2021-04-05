507ffb376ea7fd69c387dea6086c0e1a7cb9189c
André Varandas <andre.m.varandas@gmail.com>
2021-04-05 21:58:33 +0100

    feat(home): adds card for quick update view

---
+++ components/dashboard/card/CardDailyUpdate.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="nc-card">
+    <div class="nc-card__header">
+      <h2 class="font-semibold text-gray-600 text-2xl mb-1">{{ title }}</h2>
+      <p class="text-gray-400">{{ subtitle }}</p>
+    </div>
+    <div class="nc-card-info nc-card__info">
+      <div class="nc-card-info__values border-red-300">
+        <span>{{ obitos }}</span>
+        <p class="text-sm text-gray-400 font-normal">Óbitos</p>
+      </div>
+      <div class="nc-card-info__values border-yellow-300">
+        <span>{{ novos }}</span>
+        <p class="text-sm text-gray-400 font-normal">Casos Novos</p>
+      </div>
+      <div class="nc-card-info__values border-green-300">
+        <span>{{ recuperados }}</span>
+        <p class="text-sm text-gray-400 font-normal">Recuperados</p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class CardDailyUpdate extends Vue {
+  @Prop({ required: true }) title!: string
+  @Prop({ required: false, default: '' }) subtitle!: string
+  @Prop({ required: true }) recuperados!: number
+  @Prop({ required: true }) novos!: number
+  @Prop({ required: true }) obitos!: number
+}
+</script>
+
+<style lang="scss" scoped>
+.nc-card {
+  min-width: 350px;
+  @apply bg-white px-8 py-6 border-b-4 rounded-2xl shadow-lg;
+
+  &__header {
+    @apply flex flex-col items-start;
+  }
+}
+
+.nc-card-info {
+  @apply flex justify-around mt-6;
+
+  &__values {
+    @apply flex flex-col flex-1 items-center border-t-4 py-2 font-bold text-gray-600 text-xl;
+  }
+
+  &__values:not(:first-child) {
+    @apply ml-4;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="container">
-    <h1 class="title text-white text-6xl">Hello, world.</h1>
+  <div class="nc-container">
+    <section>
+      <h1 class="text-3xl font-bold text-gray-600 mb-2">
+        Números covid-19 Portugal
+      </h1>
+      <p class="text-xl text-gray-400 mb-6">Últimos dados disponíveis</p>
+      <div class="flex flex-col sm:flex-row items-center">
+        <CardDailyUpdate
+          class="sm:w-1/2"
+          subtitle="Resumo do relatório"
+          :title="data"
+          :recuperados="recuperados"
+          :novos="confirmados"
+          :obitos="obitos"
+        />
+        <div class="sm:ml-6 w-full mt-12 mb-2 sm:mt-0 h-full">
+          <GridReport
+            :vigilancia="update.vigilancia"
+            :activos="update.ativos"
+            :internados-uci="update.internadosUci"
+            :internados-enfermaria="update.internadosEnfermaria"
+            :confirmados-m="update.confirmadosM"
+            :confirmados-f="update.confirmadosF"
+            :obitos-m="update.obitosM"
+            :obitos-f="update.obitosF"
+          />
+        </div>
+      </div>
+    </section>
+    <section>
+      <div v-if="nationalRt" class="flex flex-col sm:flex-row">
+        <CardReprodutionUpdate :reproduction="nationalRt" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
-@Component
-export default class Home extends Vue {}
+// Utils
+import { $axios } from '@/utils/api'
+
+// Interfaces
+import { IData, IDataUpdate } from '@/interfaces/http/common.interface'
+
+// Components
+import CardDailyUpdate from '~/components/dashboard/card/CardDailyUpdate.vue'
+import CardReprodutionUpdate from '~/components/dashboard/card/CardReprodutionUpdate.vue'
+import GridReport from '~/components/dashboard/grid/GridReport.vue'
+
+@Component({
+  components: { CardDailyUpdate, CardReprodutionUpdate, GridReport },
+})
+export default class Home extends Vue {
+  last = {} as IData
+  update = {} as IDataUpdate
+  data = ''
+  recuperados = 0
+  confirmados = 0
+  obitos = 0
+  nationalRt = '0'
+
+  /**
+   * Special hook for SSR that is called during server-side rendering
+   * after the component instance is created, and on the client when navigating
+   */
+  async fetch() {
+    this.last = await $axios.get('api/v1/data/last')
+    this.update = await $axios.get('api/v1/data/update')
+    this.data = this.update.data
+    this.recuperados = this.update.recuperados
+    this.confirmados = this.update.confirmados
+    this.obitos = this.update.obitos
+
+    // Carefull here, the national RT is probably updated only on the next days...
+    this.nationalRt = this.last.rtNacional
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+.nc-container {
+  @apply container mx-auto px-6;
 }
 </style>
