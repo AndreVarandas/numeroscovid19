75e8fae372fdc409c56c8eaea4f7cddb60269289
André Varandas <andre.m.varandas@gmail.com>
2021-04-05 22:06:18 +0100

    feat(info): adds reproduction rt update card

---
+++ components/dashboard/card/CardReprodutionUpdate.vue
@@ -0,0 +1,14 @@
+<template>
+  <article>
+    <h1>Último R conhecido</h1>
+    <p>{{ reproduction }}</p>
+  </article>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from 'nuxt-property-decorator'
+
+export default class CardReprodutionUpate extends Vue {
+  @Prop({ required: true, default: '0' }) reproduction!: string
+}
+</script>
