2e1b14ca638c3a2d806d042ca61dc17bfcca5c09
André Varandas <andre.m.varandas@gmail.com>
2021-12-21 16:40:10 +0000

    build(target): updates nuxtjs target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 export default {
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
-
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'numeroscovid19.pt',
